b6811792dd276b5fc425194b9621d6460eb8333d
tcapraz <tuemayc@hotmail.com>
2023-08-31 10:41:11 +0200

    update conf

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,11 @@ extensions = [
     "sphinx_rtd_theme",
     "sphinx.ext.napoleon",
     "nbsphinx",
-]
+    'sphinxcontrib.bibtex'
+    ]
+    
+bibtex_bibfiles = ['refs.bib']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
